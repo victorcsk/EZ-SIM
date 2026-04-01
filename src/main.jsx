9b762985785990a5b39eb897f0a51827f444cddf
victorcsk <victorcskmellof@gmail.com>
2026-04-01 01:51:33 -0300

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EzSimWorldwideWebsite from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EzSimWorldwideWebsite />
+  </React.StrictMode>
+);
